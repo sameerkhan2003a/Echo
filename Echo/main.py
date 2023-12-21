674b6c7b0f72ab1a9f2addecafd5703753b1aebd
sameerkhan2003a <153911733+sameerkhan2003a@users.noreply.github.com>
2023-12-21 15:43:46 +0530

    Update main.py

--- Echo/main.py
+++ Echo/main.py
@@ -37,4 +37,4 @@ def index():
         result = response.choices[0].message.content
         
 
-    return render_template('index.html', result=result)
+    return render_template('Index.html', result=result)
